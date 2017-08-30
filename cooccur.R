1ed463a2be6018250585ee260952ffbf979ee170
roxannef <roxannefieldhouse@gmail.com>
2017-08-30 10:47:01 -0700

    Created the presence absence table and tried to use it in the cooccur function

--- cooccur.R
+++ cooccur.R
@@ -11,17 +11,18 @@ pscooccur <- import_biom("16S_otu_table_newjson.biom")
 ```
 
 
-Cutting down the information (remove samples with 1000 or less and then making it presence absence):
+Cutting down the information -Grouping it by family, remove samples with 1000 or less (if its just looking at family abundance should I increase this number?), and then making it presence absence:
 ```{r}
-pscooccur_pruned <- prune_samples(names(which(sample_sums(pscooccur) >= 100)),pscooccur)
-pscooccur_pruned[pscooccur_pruned > 0] <- 1
+pscooccur.genus <- tax_glom(pscooccur, taxrank = "Genus")
+pscooccur.family <- tax_glom(pscooccur.genus, taxrank = "Family")
+pscooccur_pruned <- prune_samples(names(which(sample_sums(pscooccur.family) >= 0)),pscooccur)
+pscooccur_pruned <-subset_samples(pscooccur_pruned, Tissue != "DNA_control")
+pscooccur_pruned_pa <- transform_sample_counts(pscooccur_pruned,function(x)1*(x>0))
 ```
 
-Running the cooccur function:
+Running the cooccur function - gives me error that the argument is length 0:
 ```{r}
-##Using R python- python.call("filter_otus_from_otu_table.py -i 16S_otu_table_newjson.biom -o 16S_otu_table_newjson_no_singletons.biom -n 2")
-
-cooccur_16S <- cooccur(mat = pscooccur_pruned,
+cooccur_16S <- cooccur(mat = pscooccur_pruned_pa,
                       type = "spp_site",
                       thresh = TRUE,
                       spp_names = TRUE
