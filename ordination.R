bb7464d0b4e5cb997deca355562affbeb9225b6b
roxannef <roxannefieldhouse@gmail.com>
2017-10-25 16:34:56 -0700

    10/25/17

--- ordination.R
+++ ordination.R
@@ -1,24 +1,37 @@
 #Import data
 readRDS(file = "pscooccur1.rds")
 
-pscooccur1_ord <-phyloseq::ordinate(pscooccur1, method = "DCA", distance = "bray")
+#What is the difference between methods... NMDS vs DCA
+
+pscooccur1_ord <- phyloseq::ordinate(pscooccur1, method = "DCA", distance = "bray")
 names(pscooccur1_ord)
 names(sample_data(pscooccur1))
 
 col.pal <- wes_palette(n = 6, name = "Darjeeling2", type = "continuous")
 palette(col.pal)
 #plot(pscooccur1_ord$rproj[,1:2], col=sample_data(pscooccur1)$type, pch=16)
+#ordihull(pscooccur1_ord$rproj[,1:2], sample_data(pscooccur1)$type, col=sample_data(pscooccur1)$type)
+#legend("topright", 
+#      legend = levels(sample_data(pscooccur1)$type), 
+#     pch=16,
+#    col = as.factor(levels(sample_data(pscooccur1)$type)))
+
+#pscooccur1_ord1<- ordinate(pscooccur1, method ="NMDS", pscooccur1_dis)
+
 plot_ordination(pscooccur1, pscooccur1_ord, color = "type")
 head(sample_data(pscooccur1))
 
-ordihull(pscooccur1_ord$rproj[,1:2], sample_data(pscooccur1)$type, col=sample_data(pscooccur1)$type)
-legend("topright", 
-       legend = levels(sample_data(pscooccur1)$type), 
-       pch=16,
-       col = as.factor(levels(sample_data(pscooccur1)$type)))
+
+#look into using vegan-adonis to determine statistical significance
+pscooccur1_dis <- phyloseq::distance(pscooccur1, method = "bray")
+df <- as(sample_data(pscooccur1),"data.frame")
+adonis(pscooccur1_dis ~type, df)
+
+
+
 
 #determining statistical signficance of type on distance
-tmp <- vegdist(otu_table(pscooccur), method = "bray")
+tmp <- vegdist(otu_table(pscooccur1), method = "bray")
 
 # taxa by type
 pscooccur1_bar <- merge_samples(pscooccur1, "type")
